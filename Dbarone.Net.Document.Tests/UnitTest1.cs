f2501ba02925356173b43253c5769cb13eb2873a
davidbarone <davidbarone@live.com>
2023-03-25 16:48:40 +1100

    Created.

---
+++ Dbarone.Net.Document.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Dbarone.Net.Document.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Dbarone.Net.Document/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Dbarone.Net.Document;
+public class Class1
+{
+
+}
