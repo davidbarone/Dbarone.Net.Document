76d426cb87a96ab540375f16e799c4dc7f854ccc
davidbarone <davidbarone@live.com>
2024-02-17 16:02:37 +1100

    schema validation tests

--- Dbarone.Net.Document.Tests/Schema/Schema.Tests.cs
+++ Dbarone.Net.Document.Tests/Schema/Schema.Tests.cs
@@ -1,13 +1,25 @@
 using Dbarone.Net.Document;
 
-public class SchemaTests {
+public class SchemaTests
+{
 
     [Theory]
     [InlineData(DocumentType.Int32, false, (int)123)]
     [InlineData(DocumentType.Int32, true, null)]
-    public void TestValidation(DocumentType schemaType, bool allowNull, object? value) {
+    public void TestValidationSuccess(DocumentType schemaType, bool allowNull, object? value)
+    {
         DocumentValue document = new DocumentValue(value);
         SchemaElement schema = new SchemaElement(schemaType, allowNull);
         Assert.True(schema.Validate(document));
+    }
+
+    [Theory]
+    [InlineData(DocumentType.Int32, false, null)]
+    [InlineData(DocumentType.Int32, false, "foobar")]
+    public void TestValidationFailure(DocumentType schemaType, bool allowNull, object? value)
+    {
+        DocumentValue document = new DocumentValue(value);
+        SchemaElement schema = new SchemaElement(schemaType, allowNull);
+        Assert.Throws<Exception>(() => schema.Validate(document));
     }
 }
